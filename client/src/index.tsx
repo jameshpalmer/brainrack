79cbb23941f978ba03bed5ee04c97cf2ce296e8b
jameshpalmer <jameshpalmer0@gmail.com>
2024-08-30 18:29:48 +0100

    Initial commit Configure for pnpm and biome

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 async function init() {
-  function Root() {
-    return <div>helo world!</div>;
-  }
+	function Root() {
+		return <div>helo world!</div>;
+	}
 
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      <Root />
-    </React.StrictMode>,
-  );
+	ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+		<React.StrictMode>
+			<Root />
+		</React.StrictMode>,
+	);
 }
 
 await init();
